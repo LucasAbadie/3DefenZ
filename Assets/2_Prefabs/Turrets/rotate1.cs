cca807a28e23b0eb0b0dcbb577ef49ee1c940c58
Lucas Alexandre <xomor97118@gmail.com>
2018-03-17 18:26:14 +0100

    update tirs + add pitouwer

---
+++ Assets/2_Prefabs/Turrets/rotate1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate1 : MonoBehaviour {
+    public int vitesse;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(vitesse, 0, 0 * Time.deltaTime); //rotate de x degrés par seconde
+    }
+}
